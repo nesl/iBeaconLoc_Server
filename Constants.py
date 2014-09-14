d9cf8e6274613a7493a27a4b4e5c08f853e73a34
pdmart <pdmart@gmail.com>
2014-09-14 10:23:50 -0700

    removed old files

--- Constants.py
+++
@@ -1,9 +0,0 @@
-# ===== TCP/IP COMMANDS =====
-# who is connecting?
-CMD_TYPE_BEACON = chr(0)
-CMD_TYPE_MOBILE = chr(1)
-# what is the request?
-CMD_NULL = chr(0)
-CMD_REQUESTKEY = chr(1)
-CMD_REQUESTPATHS = chr(2)
-
